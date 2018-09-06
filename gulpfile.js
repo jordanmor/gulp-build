30ba6834d1c3e7e5bb7dc09e63f5bd17f9acbecf
Jordan Mor <jordanmor@gmail.com>
2018-09-06 06:25:22 -0400

    Compile SCSS files into CSS, then concatenate and minify

--- gulpfile.js
+++ gulpfile.js
@@ -2,12 +2,18 @@ const gulp = require('gulp');
 const uglify = require('gulp-uglify')
 const concat = require('gulp-concat');
 const sourcemaps = require('gulp-sourcemaps');
+const sass = require('gulp-sass');
+const cleanCSS = require('gulp-clean-css');
 
 const paths = {
   scripts: {
     src: 'src/js/**/*.js',
     dest: 'dist/scripts/'
-  }
+  },
+  styles: {
+    src: 'src/sass/**/*.scss',
+    dest: 'dist/styles/'
+  },
 };
 
 gulp.task('scripts', () => {
@@ -17,4 +23,14 @@ gulp.task('scripts', () => {
     .pipe(uglify())
     .pipe(sourcemaps.write('./maps'))
     .pipe(gulp.dest(paths.scripts.dest));
+});
+
+gulp.task('styles', () => {
+  return gulp.src(paths.styles.src)
+    .pipe(sourcemaps.init())
+    .pipe(sass())
+    .pipe(concat('all.min.css'))
+    .pipe(cleanCSS())
+    .pipe(sourcemaps.write('./maps'))
+    .pipe(gulp.dest(paths.styles.dest));
 });
